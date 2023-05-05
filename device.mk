296590d65fb869e48f19127a66b52f408c8fa728
Edward Wu <bluehome.wu@gmail.com>
2023-05-05 21:16:08 +0800

    tiffany: Arrow-OS ify
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+# DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-arrow
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -70,10 +70,6 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8953
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/tiffany/tiffany-vendor.mk)
